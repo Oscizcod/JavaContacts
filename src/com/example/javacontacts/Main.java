7c314ce255a60265a2d65cafe3d30626ebf7dc42
Pravin <pravin_muruhaya@hotmail.com>
2022-10-27 21:13:57 +1100

    Add swtich block for main menu and contact menu - Quit app. Return from contact to main menu.

--- src/com/example/javacontacts/Main.java
+++ src/com/example/javacontacts/Main.java
@@ -1,9 +1,81 @@
 package com.example.javacontacts;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        main_menu();
+        // Initialize flag
+        boolean appRunning = true;
+
+        while (appRunning) {
+            main_menu();
+
+            // Get user input for main menu option
+            Scanner scanner = new Scanner(System.in);
+            int mainMenuInp;
+
+            try {
+                mainMenuInp = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Error! Please enter an option from the menu.\n");
+                continue;
+            }
+
+            switch (mainMenuInp) {
+                // Accessing contacts
+                case  1:
+                    boolean mainMenuRunning = true;
+
+                    while (mainMenuRunning) {
+                        contact_menu();
+
+                        int contactMenuInp;
+
+                        try {
+                            contactMenuInp = scanner.nextInt();
+                        } catch (Exception e) {
+                            System.out.println("Error! Please enter an option from the menu.\n");
+                            continue;
+                        }
+
+                        switch (contactMenuInp) {
+                            case 1:
+                                // Show all contacts
+                                break;
+                            case 2:
+                                // Add a new contact
+                                break;
+                            case 3:
+                                // Search for contact
+                                break;
+                            case 4:
+                                // Delete contact
+                                break;
+                            case 5:
+                                // Return to main menu
+                                mainMenuRunning = false;
+                                break;
+                            default:
+                                System.out.println("Error! Please enter an option from the menu.");
+                        }
+                    }
+
+                    break;
+
+                // Accessing messages
+                case 2:
+                    message_menu();
+                    break;
+
+                // Quit application
+                case 3:
+                    appRunning = false;
+                    break;
+                default:
+                    System.out.println("Error! Please enter one of the options provided in the menu.\n");
+            }
+        }
     }
     public static void main_menu() {
         System.out.println("Welcome to the phone contact app!\n");
